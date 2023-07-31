8bfd0712fa80984b2c2f143a6c95efb96620135c
Falco Gerritsjans <alias.fcb319@falco.pro>
2023-07-31 18:58:51 +0200

    feat: improve vehicle controller

--- index.js
+++ index.js
@@ -12,77 +12,83 @@ const config = JSON.parse(
   ).toString()
 );
 
-const dcs = Object.values(config.wheels).map(wheel => {
-  return wheel.dc
-})
-
-function killSwitch () {
-  motorHat.dcs.forEach((dc) => {
-    dc.stop()
-    on = false
-  })
-}
-console.log('dcs', dcs)
-
 const stick = new Joystick('/dev/input/js0', { mappingFn: stadiaMapper })
 
 const maxTrigger = Math.pow(2,16) - 1
 
-let on = false;
+class Driver {
+  powered = false
+  motorHat = undefined
+
+  constructor(config) {
+    this.config = config
+    const dcs = Object.values(config.wheels).map(wheel => {
+      return wheel.dc
+    })
+
+    let spec = {
+      address: 0x60,
+      steppers: [],
+      dcs: dcs,
+      servos: []
+    }
+
+    this.motorHat = MotorHat(spec)
+    this.motorHat.init()
+  }
+
+  forward(speed) {
+    if (speed > 1 ) return
+    console.log('current speed', speed)
+
+    if (!this.powered) {
+      Object.values(this.config.wheels).forEach((wheel, index) => {
+        const dc = this.motorHat.dcs[index]
+        dc.run(wheel.orientation === 'forwards' ? 'fwd' : 'back');
+      })
+
+      this.powered = true // race condition fail
+    }
+
+    this.motorHat.dcs.forEach((dc) => {
+      dc.setSpeed(speed)
+    })
+  }
+
+  stop() {
+    this.motorHat.dcs.forEach((dc) => {
+      dc.stop()
+    })
+
+    this.powered = false // race condition fail
+  }
+
+  // unexpected events
+  kill() {
+    this.stop()
+  }
+}
+
+const vehicle = new Driver(config)
+
 stick.on('update', (ev) => {
   console.log(ev)
   if (ev.name === 'RIGHT_TRIGGER') {
     if (ev.value > 0) {
       const speed = 1 / (maxTrigger - 1) * ev.value
-      console.log('current speed', speed)
-
-      if (!on) {
-        Object.values(config.wheels).forEach(wheel => {
-          const dc = motorHat.dcs[wheel.dc]
-          dc.runSync(wheel.orientation === 'forwards' ? 'fwd' : 'back');
-        })
-
-        on = true // race condition fail
-      }
-
-      motorHat.dcs.forEach((dc) => {
-        dc.setSpeed(speed)
-      })
+      vehicle.forward(speed)
     } else {
-      killSwitch()
+      vehicle.stop()
     }
   }
 })
 
 stick.on('disconnect', () => {
   console.log('disconnected')
-  killSwitch()
+  vehicle.kill()
 })
 
 stick.on('error', (err) => {
   console.log('Error!', err)
-  killSwitch()
+  vehicle.kill()
 })
-
-let spec = {
-  address: 0x60,
-  steppers: [],
-  dcs: ['M1', 'M2', 'M3', 'M4'],
-  servos: []
-};
-
-const motorHat = MotorHat(spec);
-
-// Since MotorHat 2.0, the instance needs to be initialized.
-// This is to enable async initialization, feel free to open an issue if this i>
-motorHat.init();
-
-
-
-// Start dc motor forward (by default at 100% speed)
-// motorHat.dcs[0].runSync('fwd');
-
-
-// setTimeout(() => { motorHat.dcs[0].setSpeedSync(50); }, 1000);
-// setTimeout(() => { motorHat.dcs[0].runSync('back'); }, 2000);
-// setTimeout(() => { motorHat.dcs[0].stopSync();  }, 3000);
