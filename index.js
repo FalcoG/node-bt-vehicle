0ebbca43336938c4e33f87a106c616428d881d2f
Falco Gerritsjans <alias.fcb319@falco.pro>
2023-07-31 17:25:16 +0200

    feat: add basic movement control

--- index.js
+++ index.js
@@ -1,12 +1,78 @@
 import Joystick from '@hkaspy/joystick-linux'
-import config from './config.json' assert { type: "json" }
+import MotorHat from 'motor-hat'
+import config from './config.json' assert { type: 'json' }
 import { stadiaMapper } from './lib/stadia-input-map.js'
 
 const dcs = Object.values(config.wheels).map(wheel => {
   return wheel.dc
 })
 
+function killSwitch () {
+  motorHat.dcs.forEach((dc) => {
+    dc.stop()
+    on = false
+  })
+}
 console.log('dcs', dcs)
 
-const stick = new Joystick("/dev/input/js0", { mappingFn: stadiaMapper })
-stick.on("update", (ev) => console.log(ev))
+const stick = new Joystick('/dev/input/js0', { mappingFn: stadiaMapper })
+
+const maxTrigger = Math.pow(2,16) - 1
+
+let on = false;
+stick.on('update', (ev) => {
+  console.log(ev)
+  if (ev.name === 'RIGHT_TRIGGER') {
+    if (ev.value > 0) {
+      const speed = 1 / (maxTrigger - 1) * ev.value
+
+      if (!on) {
+        Object.values(config.wheels).forEach(wheel => {
+          const dc = motorHat.dcs[wheel.dc]
+          dc.runSync(wheel.orientation === 'forwards' ? 'fwd' : 'back');
+        })
+
+        on = true // race condition fail
+      }
+
+      motorHat.dcs.forEach((dc) => {
+        dc.setSpeed(speed)
+      })
+    } else {
+      killSwitch()
+    }
+  }
+})
+
+stick.on('disconnect', () => {
+  console.log('disconnected')
+  killSwitch()
+})
+
+stick.on('error', (err) => {
+  console.log('Error!', err)
+  killSwitch()
+})
+
+let spec = {
+  address: 0x60,
+  steppers: [],
+  dcs: ['M1', 'M2', 'M3', 'M4'],
+  servos: []
+};
+
+const motorHat = MotorHat(spec);
+
+// Since MotorHat 2.0, the instance needs to be initialized.
+// This is to enable async initialization, feel free to open an issue if this i>
+motorHat.init();
+
+
+
+// Start dc motor forward (by default at 100% speed)
+// motorHat.dcs[0].runSync('fwd');
+
+
+// setTimeout(() => { motorHat.dcs[0].setSpeedSync(50); }, 1000);
+// setTimeout(() => { motorHat.dcs[0].runSync('back'); }, 2000);
+// setTimeout(() => { motorHat.dcs[0].stopSync();  }, 3000);
