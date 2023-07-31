4895ed7bf2d1b06ebfc2a230fb53669c61ac3b1e
Falco Gerritsjans <alias.fcb319@falco.pro>
2023-07-31 15:31:45 +0200

    feat: add stadia input mapper

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-import config from './config.json' assert { type: "json" };
+import Joystick from '@hkaspy/joystick-linux'
+import config from './config.json' assert { type: "json" }
+import { stadiaMapper } from './lib/stadia-input-map.js'
 
 const dcs = Object.values(config.wheels).map(wheel => {
   return wheel.dc
 })
 
 console.log('dcs', dcs)
+
+const stick = new Joystick("/dev/input/js0", { mappingFn: stadiaMapper })
+stick.on("update", (ev) => console.log(ev))
