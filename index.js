578acddc95821514e6c317d8502c1b428d774c8f
Falco Gerritsjans <alias.fcb319@falco.pro>
2023-08-01 22:15:18 +0200

    refactor: improve motor direction update

--- index.js
+++ index.js
@@ -12,18 +12,16 @@ const config = JSON.parse(
   ).toString()
 );
 
-const stick = new Joystick('/dev/input/js0', { mappingFn: stadiaMapper })
-
-const maxTrigger = Math.pow(2,16) - 1
-
 const debuggerCallback = (err, result) => {}
 
 class Driver {
   powered = false
   motorHat = undefined
+  wheels = {}
   direction = {
     forwards: 0,
     backwards: 0,
+    rotation: 0,
     heading: 'forwards',
     update: () => {
       const x = -this.direction.backwards + this.direction.forwards
@@ -33,28 +31,31 @@ class Driver {
       if (x > 0) {
         //fwd
         if (this.direction.heading !== 'forwards') {
-          Object.values(this.config.wheels).forEach((wheel, index) => {
-            const dc = this.motorHat.dcs[index]
-            dc.run(wheel.orientation === 'forwards' ? 'fwd' : 'back', debuggerCallback);
-          })
-
-          this.direction.heading = 'forwards'
+          this.direction.setMotorDirection('forwards')
         }
       } else if (x < 0) {
         //rwd
         if (this.direction.heading !== 'backwards') {
-          Object.values(this.config.wheels).forEach((wheel, index) => {
-            const dc = this.motorHat.dcs[index]
-            dc.run(wheel.orientation === 'forwards' ? 'back' : 'fwd', debuggerCallback);
-          })
-
-          this.direction.heading = 'backwards'
+          this.direction.setMotorDirection('backwards')
         }
       }
 
-      this.motorHat.dcs.forEach((dc) => {
-        dc.setSpeed(Math.abs(x * 100), debuggerCallback)
+      Object.keys(this.wheels).forEach((key) => {
+        const wheel = this.wheels[key]
+
+        const speedModifier = 1
+        wheel.driver.setSpeed(Math.abs(x * 100 * speedModifier), debuggerCallback)
       })
+    },
+    setMotorDirection: (forward = 'forwards') => {
+      Object.values(this.config.wheels).forEach((wheel, index) => {
+        const dc = this.motorHat.dcs[index]
+        const front = forward === 'forwards' ? 'fwd' : 'back'
+        const back = forward === 'forwards' ? 'back' : 'fwd'
+        dc.run(wheel.orientation === 'forwards' ? front : back, debuggerCallback);
+      })
+
+      this.direction.heading = 'forwards'
     }
   }
 
@@ -73,6 +74,13 @@ class Driver {
 
     this.motorHat = MotorHat(spec)
     this.motorHat.init()
+
+    Object.keys(config.wheels).map((key, index) => {
+      this.wheels[key] = {
+        config: config.wheels[key],
+        driver: this.motorHat.dcs[index]
+      }
+    })
   }
 
   stop() {
@@ -81,8 +89,10 @@ class Driver {
     })
 
     this.direction.heading = 'idle'
+    this.direction.forwards = 0
+    this.direction.backwards = 0
 
-    this.powered = false // race condition fail
+    this.powered = false // todo: prevent race condition with dcs loop
   }
 
   // unexpected events
@@ -93,6 +103,10 @@ class Driver {
 
 const vehicle = new Driver(config)
 
+const stick = new Joystick('/dev/input/js0', { mappingFn: stadiaMapper })
+const maxTrigger = Math.pow(2,16) - 1
+const maxAxis = Math.pow(2,16)/2 - 1
+
 stick.on('update', (ev) => {
   // console.log(ev)
   if (ev.name === 'RIGHT_TRIGGER') {
@@ -100,6 +114,9 @@ stick.on('update', (ev) => {
     vehicle.direction.update()
   } else if (ev.name === 'LEFT_TRIGGER') {
     vehicle.direction.backwards = 1 / (maxTrigger - 1) * ev.value
+    vehicle.direction.update()
+  } else if (ev.name === 'LEFT_STICK_Y') {
+    vehicle.direction.rotation = 1 / (maxAxis) * ev.value
     vehicle.direction.update()
   }
 })
